b7dc625c6245dced44a1c0bd2f629feda7408ac6
hvignatti <hvignatti.developer@gmail.com>
2022-08-04 22:55:30 -0300

    eventos listos

--- app.js
+++ app.js
@@ -1,43 +1,81 @@
 
+let formDolar = document.getElementById("formDolar");
+formDolar.addEventListener("submit", resDolar);
 
-
-
-
-
-
-let cotizacionDolar = document.getElementById("cotizacionDolar");
-cotizacionDolar.addEventListener("click", resDolar);
+let valorDolar=285
 
 function resDolar (e){
     e.preventDefault();
-    console.log("cotizacion dolar")
-    let divd = document.getElementById("cotizacionDolar")
-    divd.innerHTML = ` <p> La cotizacion del dolar es de $283</p>`
-    document.body.append
+
+    let formDolar = e.target
+
+
+    console.log(formDolar.children[1].value)
+
+    let cantidadCompra = formDolar.children[1].value
+    
+    mostrarResultado (cantidadCompra, valorDolar)
+}
+
+function mostrarResultado(cantidadCompra,valorDolar){
+    let div = document.createElement("div")
+    totalDolar.innerHTML = `<h2>El total es de ${cantidadCompra * valorDolar}</h2>`;
+
+    document.body.append(div)
 
 }
 
-let cotizacionEuro = document.getElementById("cotizacionEuro");
-cotizacionEuro.addEventListener("click", resEuro);
+let formEuro = document.getElementById("formEuro");
+formEuro.addEventListener("submit", resEuro);
+
+let valorEuro= 335
 
 function resEuro (e){
-    e.preventDefault();
-    console.log("cotizacion euro")
-    let dive = document.getElementById("cotizacionEuro")
-    dive.innerHTML = ` <p> La cotizacion del euro es de $300</p>`
-    document.body.append
+    e.preventDefault ();
+
+    let formEuro = e.target
+
+    let cantidadCompraEuro = formEuro.children[1].value
+    mostrarResultadoEuro(cantidadCompraEuro, valorEuro)
+}
+
+function mostrarResultadoEuro ( cantidadCompraEuro, valorEuro){
+
+    let div = document.createElement("div")
+    totalEuro.innerHTML = `<h2>El total es de ${cantidadCompraEuro * valorEuro}</h2>`;
+
+    document.body.append(div)
+
+
 
 }
 
-let cotizacionReal = document.getElementById("cotizacionReal");
-cotizacionReal.addEventListener("click", resReal);
+
+let formReal = document.getElementById("formReal");
+formReal.addEventListener("submit", resReal);
+
+let valorReal= 40
 
 function resReal (e){
-    e.preventDefault();
-    console.log("cotizacion real")
-    let divr = document.getElementById("cotizacionReal")
-    divr.innerHTML = ` <p> La cotizacion del real es de $185</p>`
-    document.body.append
+    e.preventDefault ();
+
+    let formReal = e.target
+
+    let cantidadCompraReal = formReal.children[1].value
+    mostrarResultadoReal(cantidadCompraReal, valorReal)
+}
+
+function mostrarResultadoReal ( cantidadCompraReal, valorReal){
+
+    let div = document.createElement("div")
+    totalReal.innerHTML = `<h2>El total es de ${cantidadCompraReal * valorReal}</h2>`;
+
+    document.body.append(div)
+
 
 
 }
+
+
+
+
